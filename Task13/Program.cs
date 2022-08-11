de0adb48152c838c8bfa2cea6a0fc109a6e693a8
Masterov_IN <masterov_in@mail.ru>
2022-08-11 19:34:56 +0300

    Task13 done

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -7,3 +7,20 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+if (number < 100) Console.Write("Нет третьего числа");
+if (number > 100)
+{
+    for (int i = number; i > 0; i = i / 10)
+    {
+        if (i > 999)
+            continue;
+        else if (i < 1000 && i > 99) result = i % 10;
+    }
+    Console.WriteLine($"Третья цифра числа {number}: {result % 10}");
+}
