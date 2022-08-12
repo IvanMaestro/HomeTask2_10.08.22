8ca1f857a03b8fc4060b68bf5f8990ff80e6dd96
Masterov_IN <masterov_in@mail.ru>
2022-08-12 10:22:45 +0300

    Task15 done

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -6,3 +6,37 @@
 // 7 -> да
 // 1 -> нет
 
+Console.Write("Введите целое число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string day = " ";
+
+switch (number)
+{
+    case 1:
+        day = "Не выходной";
+        break;
+    case 2:
+        day = "Не выходной";
+        break;
+    case 3:
+        day = "Не выходной";
+        break;
+    case 4:
+        day = "Не выходной";
+        break;
+    case 5:
+        day = "Не выходной";
+        break;
+    case 6:
+        day = "Выходной";
+        break;
+    case 7:
+        day = "Выходной";
+        break;
+    default:
+        day = $"Нет дня недели с номером {number}";
+        break;
+
+}
+Console.Write(day);
